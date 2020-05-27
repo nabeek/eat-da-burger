9806de26f45d26d87dc2d57c9fe789a2ba922c6e
nabeek <nabeek@gmail.com>
2020-05-27 17:58:02 -0600

    App fully styled

--- config/orm.js
+++ config/orm.js
@@ -48,8 +48,6 @@ const orm = {
     queryString += printQuestionMarks(vals.length);
     queryString += ") ";
 
-    console.log(queryString);
-
     connection.query(queryString, vals, function (err, result) {
       if (err) {
         throw err;
@@ -67,7 +65,6 @@ const orm = {
     queryString += " WHERE ";
     queryString += condition;
 
-    console.log(queryString);
     connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
